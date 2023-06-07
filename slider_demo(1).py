98a4ca6a1d0f9e61d950a99fece9ae26947debea
Andbwol <115871119+Andbwol@users.noreply.github.com>
2023-06-07 10:26:40 +0300

    Add files via upload

---
+++ slider_demo(1).py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv('housing.csv')
+
+if st.header('Выберите размер обучающей выборки'):
+    size = st.slider('Размер обучающей выборки?', 0.1, 0.2, 0.4)
+    st.write("Размер обучающей выборки", size)
+
+if st.button('Отобразить первые пять строк'):
+    st.write(df.head())
+
+if st.button('Обучить модель'):
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('MEDV', axis=1), df['MEDV'], test_size=0.2, random_state=42)
+    st.write('Разделили данные и передали в обучение')
+    regr_model = XGBRegressor()
+    regr_model.fit(X_train, y_train)
+    pred = regr_model.predict(X_test)
+    st.write('Обучили модель, MAE = ' + str(mean_absolute_error(y_test, pred)))
